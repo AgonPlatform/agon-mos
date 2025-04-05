970e840056f529dc88ebf8a9597ff63ad04a76e8
Steve Sims <steve@stevesims.dev>
2025-04-05 18:54:34 +0100

    bump version to 3.0 beta 1
    also temporarily removes the version subtitle, as we’ll give the proper 3.0.0 release a new one

--- src/version.h
+++ src/version.h
@@ -4,10 +4,10 @@
 #define		VERSION_MAJOR		3
 #define		VERSION_MINOR		0
 #define		VERSION_PATCH		0
-#define		VERSION_CANDIDATE	4			// Optional
-#define		VERSION_TYPE		"Alpha "	// RC, Alpha, Beta, etc.
+#define		VERSION_CANDIDATE	1			// Optional
+#define		VERSION_TYPE		"Beta "     // RC, Alpha, Beta, etc.
 
 #define		VERSION_VARIANT		"Console8"
-#define     VERSION_SUBTITLE    "Rainbow"
+// #define     VERSION_SUBTITLE    ""
 
 #endif // VERSION_H
