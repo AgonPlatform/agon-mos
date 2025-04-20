3eeaa3caec3a6575bd6ff4a5f0937f2c1a8758fe
Steve Sims <steve@stevesims.dev>
2025-04-20 15:38:21 +0100

    version bump to 3.0

--- src/version.h
+++ src/version.h
@@ -4,10 +4,10 @@
 #define		VERSION_MAJOR		3
 #define		VERSION_MINOR		0
 #define		VERSION_PATCH		0
-#define		VERSION_CANDIDATE	3			// Optional
-#define		VERSION_TYPE		"Beta "     // RC, Alpha, Beta, etc.
+// #define		VERSION_CANDIDATE	1			// Optional
+// #define		VERSION_TYPE		"Beta "     // RC, Alpha, Beta, etc.
 
 #define		VERSION_VARIANT		"Platform"
-// #define     VERSION_SUBTITLE    ""
+#define     VERSION_SUBTITLE    "Arthur"
 
 #endif // VERSION_H
